92bc58fd108adba6089b5f9358db9e198b8ce870
Wyelier <sleptsov2004@gmail.com>
2023-01-17 10:26:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/gitsearchbyusername/GitHubService.java
@@ -0,0 +1,19 @@
+package com.example.gitsearchbyusername;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface GitHubService {
+    // GET /search/users
+
+    @GET("/search/users")
+    Call<GitResult> getUsers(@Query("q") String name);
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
---
+++ app/src/main/java/com/example/gitsearchbyusername/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.gitsearchbyusername;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar mProgressBar;
+    private TextView mTextView;
+    private EditText mEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textView);
+        mProgressBar = findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mEditText = findViewById(R.id.editText);
+    }
+
+
+    public void onClick(View view) {
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        GitHubService gitHubService = GitHubService.retrofit.create(GitHubService.class);
+        // часть слова
+        final Call<GitResult> call =
+                gitHubService.getUsers(String.valueOf(mEditText.getText()));
+
+        call.enqueue(new Callback<GitResult>() {
+            @Override
+            public void onResponse(Call<GitResult> call, Response<GitResult> response) {
+                if (response.isSuccessful()) {
+                    GitResult result = response.body();
+                    // Получаем json из github-сервера и конвертируем его в удобный вид
+                    // Покажем только первого пользователя
+                    String user = "Аккаунт Github: " + result.getItems().get(0).getLogin();
+                    mTextView.setText(user);
+                    Log.i("Git", String.valueOf(result.getItems().size()));
+
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    int statusCode = response.code();
+
+                    // handle request errors yourself
+                    ResponseBody errorBody = response.errorBody();
+                    try {
+                        mTextView.setText(errorBody.string());
+                        mProgressBar.setVisibility(View.INVISIBLE);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GitResult> call, Throwable t) {
+                mTextView.setText("Что-то пошло не так " + t.getMessage());
+            }
+        });
+    }
+}
